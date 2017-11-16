2dbda75bb834d4b718f4848fe5a3fe33e964221d
jennyjaderborn <32732982+jennyjaderborn@users.noreply.github.com>
2017-11-16 13:12:36 +0100

    sessionstoragefungerarnog

--- script.js
+++ script.js
@@ -1,47 +1,40 @@
 
 $(document).ready(function(){
+    $(".forgotPassword").hide();
+    $(".button2").hide();
     if(sessionStorage.username!= null){//om nån är inne
         $("h1").hide();
         $(".medlemsSida").text(sessionStorage.username);
         $(".button").hide();
         $("input, label").hide();
         $(".button2").show();
-        //$(".medlemsSida").text(sessionStorage.username);
-    } else {
-        $("h1").show();
+        $(".forgotPassword").hide();
+    } else { 
+        
+        $(".button").click(function(){
+        
+    sessionStorage.username = "test";
+    sessionStorage.password = "password"; 
+    
+
+                    if(sessionStorage.username == $(".inputName").val() && sessionStorage.password == $(".inputPassword").val()){
+                        sessionStorage.username="Jenny Jäderborn";
+                        $(".medlemsSida").text(sessionStorage.username);                
+                        $("h1").hide();
+                        $(".button").hide();
+                        $("input, label").hide();
+                        $(".button2").show();
+                        $(".medlemsSida").show();
+                        $(".forgotPassword").hide();
+                    } 
+
+                    else {$("h1").hide();
+                            $(".forgotPassword").slideDown();
+                            $(".button2").hide();
+                    } 
+                });
+        
     }
 
-    console.log(sessionStorage.username);
-
-    //sessionStorage.username="Jenny Jäderborn";
-    
-
-
-    $(".forgotPassword").hide();
-    $(".button2").hide();
-
-        $(".button").click(function(){
-            
-        sessionStorage.username = "test";
-        sessionStorage.password = "password"; 
-        
-    
-                        if(sessionStorage.username == $(".inputName").val() && sessionStorage.password == $(".inputPassword").val()){
-                            sessionStorage.username="Jenny Jäderborn";
-                            $(".medlemsSida").text(sessionStorage.username);                
-                            $("h1").hide();
-                            $(".button").hide();
-                            $("input, label").hide();
-                            $(".button2").show();
-                            $(".medlemsSida").show();
-                        } 
-
-                        else {$("h1").hide();
-                                $(".forgotPassword").slideDown();
-                        }
-    
-                        
-                    });
-    
-                });
+});
     
